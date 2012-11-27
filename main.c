d86e251bc2cb608d361f9c025576402a8dd5bfca
jibi <jibi@paranoici.org>
2012-11-27 01:21:38 +0100

    rewrite synchronization

--- main.c
+++ main.c
@@ -21,6 +21,7 @@
 #include <fcntl.h>
 
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 enum bool {
 	false,
@@ -33,18 +34,14 @@ enum pipes {
 	 * src_reader read from this.
 	 */
 	D2S_BUF,
+	D2S_CTL,
 
 	/*
 	 * src_reader write src_fd content into this pipe,
 	 * dst_writer read from this
 	 */
 	S2D_BUF,
-
-	/*
-	 * dst_reader ctl pipe
-	 */
-	DR_CTL,
-
+	S2D_CTL,
 
 	N_PIPES
 };
@@ -52,6 +49,7 @@ enum pipes {
 char *src, *dst;
 unsigned int bs;
 char smart_mode;
+char null_char;
 
 void
 fatal(int code, const char *msg, ...) {
@@ -99,7 +97,7 @@ parse_arg(char *arg) {
 }
 
 void
-open_fd(int *fd_src, int *fd_dst) {
+open_fd(int *fd_src, int *fd_dst_r, int *fd_dst_w) {
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
 
 	*fd_src = (src ? open(src, O_RDONLY) : STDIN_FILENO);
@@ -110,30 +108,32 @@ open_fd(int *fd_src, int *fd_dst) {
 	}
 
 	if (!dst) {
-		*fd_dst = STDOUT_FILENO;
+		*fd_dst_w = STDOUT_FILENO;
 	} else {
 		if (access(dst, F_OK) != -1) {
 			printf("in smart mode\n");
 			smart_mode = 1;
 		}
 
-		*fd_dst = open(dst, O_RDWR | O_CREAT, mode);
+		*fd_dst_r = open(dst, O_RDWR | O_CREAT, mode);
+		*fd_dst_w = open(dst, O_RDWR | O_CREAT, mode);
 	}
 
-	if (*fd_dst == -1) {
+	if (*fd_dst_r == -1 || *fd_dst_w == -1) {
 		fatal(2, "Cannot open %s", dst);
 	}
 }
 
 void
-src_reader(int fd_src, int d2s_r, int s2d_w, int dr_ctl_r) {
+src_reader(int fd_src, int d2s_buf_r, int d2s_ctl_w, 
+    int s2d_ctl_r, int s2d_buf_w) {
+
 	char *buf_src, *buf_dst;
 
 	int blocks;
-	ssize_t count;
-	ssize_t negative_count = -1;
+	ssize_t count, count2;
 	int dr_running = 1;
-	char ctl;
+
 
 	buf_src = malloc(bs * sizeof(char));
 	buf_dst = malloc(bs * sizeof(char));
@@ -142,26 +142,21 @@ src_reader(int fd_src, int d2s_r, int s2d_w, int dr_ctl_r) {
 
 	while ((count = read(fd_src, buf_src, bs)) > 0) {
 		char diff;
-
 		diff = true;
+
 		if (dr_running && smart_mode) {
-			/* syncrhonize dst reader process */
-			read(dr_ctl_r, &ctl, sizeof(char));
 
-			if (ctl == 'e') {
+			count2 = read(d2s_buf_r, buf_dst, bs);
+			write(d2s_ctl_w, &null_char, sizeof(char));
+
+			if (count != count2) {
 				dr_running = 0;
-				printf("[+] quitting smart mode at block: %d\n",
-				     blocks);
+				printf("[+] quitting smart mode at block: %d\n", blocks);
 			} else {
-
-				/* read from dst file pipe */
-				read(d2s_r, buf_dst, count);
-
 				diff = false;
 				for (int i = 0; i < count; i++) {
 					if (buf_src[i] != buf_dst[i]) {
-						printf("[+] diff at block: %d\n",
-						     blocks);
+						printf("[+] diff at block: %d\n", blocks);
 						diff = true;
 						break;
 					}
@@ -170,61 +165,65 @@ src_reader(int fd_src, int d2s_r, int s2d_w, int dr_ctl_r) {
 		}
 
 		if (diff) {
-			/* tell dst writer process current block and count */
-			write(s2d_w, &count, sizeof(ssize_t));
-			write(s2d_w, &blocks, sizeof(int));
+			/* wait for dst writer to be ready */
+			read(s2d_ctl_r, &null_char, sizeof(char));
 
-			write(s2d_w, buf_src, count);
-
+			/* tell dst writer process current block */
+			write(s2d_buf_w, &blocks, sizeof(int));
+			write(s2d_buf_w, buf_src, count);
 		}
 		blocks++;
 	}
-	write(s2d_w, &negative_count, sizeof(ssize_t));
+
+	close(s2d_buf_w);
 }
 
 void
-dst_reader(int fd_dst, int d2s_w, int dr_ctl_w) {
-	char ctl = 'r';
+dst_reader(int fd_dst, int d2s_ctl_r, int d2s_buf_w) {
+	/* move a block from dst file to d2s_buf pipe */
+	while (splice(fd_dst, NULL, d2s_buf_w, NULL, bs, 0) > 0) {
 
-	/* move a block from dst file to d2s pipe */
-	while (splice
-	       (fd_dst, NULL, d2s_w, NULL, bs, SPLICE_F_NONBLOCK) > 0) {
-
-		/* wait src reader process */
-		write(dr_ctl_w, &ctl, sizeof(char));
+		/* wait src reader to read next block */
+		read(d2s_ctl_r, &null_char, sizeof(char));
 	}
 
-	ctl = 'e';
-	write(dr_ctl_w, &ctl, sizeof(char));
+	close(d2s_buf_w);
 }
 
 void
-dst_writer(int fd_dst, int s2d_r) {
-	ssize_t count;
+dst_writer(int fd_src, int fd_dst, int s2d_buf_r, int s2d_ctl_w) {
 	int block;
+	struct stat stat_src, stat_dst;
 
-	/* read current block */
 	while (1) {
-		read(s2d_r, &count, sizeof(ssize_t));
+		write(s2d_ctl_w, &null_char, sizeof(char));
 
-		if (count == -1) {
-			break;
-		}
-
-		read(s2d_r, &block, sizeof(int));
-
+		/* read current block */
+		if (read(s2d_buf_r, &block, sizeof(int)) <= 0) break;
 		lseek(fd_dst, block * bs, SEEK_SET);
 
 		/* move block from s2d pipe to dst file */
-		splice(s2d_r, NULL, fd_dst, NULL, count, 0);
+		splice(s2d_buf_r, NULL, fd_dst, NULL, bs, 0);
 	}
 
-	/* TODO: truncate */
+	fstat(fd_src, &stat_src); 
+	fstat(fd_dst, &stat_dst); 
+
+	if(stat_dst.st_size > stat_src.st_size) {
+		ftruncate(fd_dst, stat_src.st_size);
+	}
+}
+
+void 
+close_pipes(int pipes[][2], enum pipes *wat, int num, int rw) {
+	for (int i = 0; i < num; i++) {
+		close(pipes[wat[i]][rw]);
+	}
 }
 
 int
 main(int argc, char *argv[]) {
-	int fd_src, fd_dst;
+	int fd_src, fd_dst_r, fd_dst_w;
 	int pipes[N_PIPES][2];
 	pid_t child1, child2;
 
@@ -240,7 +239,7 @@ main(int argc, char *argv[]) {
 		fatal(1, "Block size cannot be 0");
 	}
 
-	open_fd(&fd_src, &fd_dst);
+	open_fd(&fd_src, &fd_dst_r, &fd_dst_w);
 
 	for (int i = 0; i < N_PIPES; i++) {
 		pipe(pipes[i]);
@@ -251,15 +250,23 @@ main(int argc, char *argv[]) {
 
 		child2 = fork();
 		if (child2) {
-			src_reader(fd_src, pipes[D2S_BUF][0],
-				   pipes[S2D_BUF][1], pipes[DR_CTL][0]);
+			close_pipes(pipes, (enum pipes []) {D2S_CTL, S2D_BUF}, 2, 0);
+			close_pipes(pipes, (enum pipes []) {D2S_BUF, S2D_CTL}, 2, 1);
 
+			src_reader(fd_src, pipes[D2S_BUF][0], pipes[D2S_CTL][1], 
+			    pipes[S2D_CTL][0], pipes[S2D_BUF][1]);
 		} else {
-			dst_writer(fd_dst, pipes[S2D_BUF][0]);
+			close_pipes(pipes, (enum pipes []) {S2D_CTL, D2S_BUF, D2S_CTL}, 3, 0);
+			close_pipes(pipes, (enum pipes []) {S2D_BUF, D2S_BUF, D2S_CTL}, 3, 1);
+
+			dst_writer(fd_src, fd_dst_w, pipes[S2D_BUF][0], pipes[S2D_CTL][1]);
 		}
 
 	} else {
-		dst_reader(fd_dst, pipes[D2S_BUF][1], pipes[DR_CTL][1]);
+		close_pipes(pipes, (enum pipes []) {S2D_BUF, S2D_CTL, D2S_BUF}, 3, 0);
+		close_pipes(pipes, (enum pipes []) {S2D_BUF, S2D_CTL, D2S_CTL}, 3, 1);
+
+		dst_reader(fd_dst_r, pipes[D2S_CTL][0], pipes[D2S_BUF][1]); 
 	}
 
 	waitpid(child2, NULL, 0);
